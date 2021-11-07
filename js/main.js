6717693a557c6af4a6d8db354f677a888666ad1f
Ushlerdop <axalimon@gmail.com>
2021-11-07 17:29:45 +1000

    start

---
+++ js/main.js
@@ -0,0 +1,55 @@
+const $modalAuth = document.querySelector('.modal-auth');
+const $buttonAuth = document.querySelector('.button-auth');
+const $closeAuth = document.querySelector('.close-auth');
+const $logInForm = document.getElementById('logInForm');
+const $inputLogin = document.getElementById('login');
+const $inputPassword = document.getElementById('password');
+const $buttonOut = document.querySelector('.button-out');
+const $userName = document.querySelector('.user-name');
+
+console.log($buttonOut);
+console.log($userName);
+
+const login = (user) => {
+    console.log(user);
+    $buttonAuth.style.display = 'none';
+    $buttonOut.style.display = 'flex';
+    $userName.style.display = 'flex';
+    $userName.textContent = user.login;
+    $modalAuth.style.display = 'none';
+}
+
+const logout = () => {
+    $buttonAuth.style.display = 'flex';
+    $buttonOut.style.display = 'none';
+    $userName.style.display = 'none';
+    $userName.textContent = '';
+    localStorage.removeItem('user');
+}
+
+$buttonAuth.addEventListener('click', () => {
+    $modalAuth.style.display = 'flex';
+})
+
+$buttonOut.addEventListener('click', () => {
+    logout();
+})
+
+$closeAuth.addEventListener('click', (e) => {
+    e.preventDefault;
+    $modalAuth.style.display = 'none';
+})
+
+$logInForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const user = {
+        login: $inputLogin.value,
+        password: $inputPassword.value,
+    }
+    localStorage.setItem('user', JSON.stringify(user));
+    login(user);
+})
+
+if (localStorage.getItem('user')) {
+    login(JSON.parse(localStorage.getItem('user')));
+}
